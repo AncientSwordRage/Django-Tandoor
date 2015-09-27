d4491ab46037799c2a2ee1c43bc9b9f7675c46ea
AncientSwordRage <pureferret@gmail.com>
2015-09-27 22:57:26 +0100

    adds category description and spice description

--- tandoor/models.py
+++ tandoor/models.py
@@ -42,6 +42,10 @@ class FoodCategory(models.Model):
     description = models.TextField(blank=True,default=None,null=True)
     spice_description = models.CharField(max_length=64,choices=SPICE_CHOICES, default='none',null=True)
     categoryType =  models.CharField(max_length=128, choices=CATEGORY_TYPE_CHOICES)
+    def get_spice_description(self):
+        if not self.get_spice_description_display():
+            return ""
+        return "("+self.get_spice_description_display()+")"
     def __str__(self):
         return self.name
    
